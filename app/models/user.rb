ded46a7713878e83d7e0baf55a61296d6badebb5
ngo quang <ngoquang78264@gmail.com>
2020-08-07 15:39:14 +0700

    Chapter 9: Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   USERS_PARAMS = %i(name email password password_confirmation).freeze
 
   validates :name,
@@ -15,6 +17,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  before_save :downcase_email
+
   class << self
     def digest string
       cost = if ActiveModel::SecurePassword.min_cost
@@ -24,11 +28,29 @@ class User < ApplicationRecord
              end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
-  before_save :downcase_email
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update remember_digest: nil
+  end
 
   private
+
   def downcase_email
     email.downcase!
   end
